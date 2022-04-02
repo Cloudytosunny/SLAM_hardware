49cc9a0ed5288f906206c9a03d928233c835ded7
Cloudytosunny <1287145695@qq.com>
2022-04-02 11:14:32 +0800

    add xsens pkg

---
+++ catkin_xsens/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/seu/catkin_xsens/src
+# Build directory: /home/seu/catkin_xsens/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("xsens_ros_mti_driver")
---
+++ catkin_xsens/build/xsens_ros_mti_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/seu/catkin_xsens/src/xsens_ros_mti_driver
+# Build directory: /home/seu/catkin_xsens/build/xsens_ros_mti_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
