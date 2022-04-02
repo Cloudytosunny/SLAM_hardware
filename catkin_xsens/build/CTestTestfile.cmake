f016e67b54fd54ffcd7de5d61c57fe864b1c8a3f
CloudyToSunny <76858453+Cloudytosunny@users.noreply.github.com>
2022-04-02 11:17:35 +0800

    Delete catkin_xsens directory

--- catkin_xsens/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seu/catkin_xsens/src
-# Build directory: /home/seu/catkin_xsens/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("xsens_ros_mti_driver")
